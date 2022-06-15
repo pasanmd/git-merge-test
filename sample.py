a148f42cc281b35c870add3e8868f028dc700e1e
pasanmd <pasanmd@gmail.com>
2022-06-15 21:49:16 +0530

    new function

--- sample.py
+++ sample.py
@@ -4,3 +4,4 @@ import os
 print("this is sample python file i used test out the merging problems")
 print("this is the secnd update tomain")
 print("critical code update")
+print("new function add")
