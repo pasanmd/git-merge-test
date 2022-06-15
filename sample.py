d705423e8ad052c49024fb3c9f6eac3cd3bc7603
pasanmd <pasanmd@gmail.com>
2022-06-15 21:50:19 +0530

    another function

--- sample.py
+++ sample.py
@@ -7,3 +7,4 @@ print("this is the second line of sample python file i used test out the merging
 =======
 print("this is the third branch created from")
 >>>>>>> Stage
+print("another fucntion add")
